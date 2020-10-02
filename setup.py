140f93452c8cc6c8a6856cd044408a99b5668e38
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-10-02 19:38:33 +0800

    试一下

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name='rimo_util',
+    version='0.0.1',
+    author='RimoChan',
+    author_email='the@librian.net',
+    description='RimoChan util.',
+    long_description='喵喵喵！',
+    long_description_content_type='text/markdown',
+    url='https://github.com/librian-center/liber-language',
+    packages=['rimo_util'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
