8e500907dc0086e385ce68b2c7e15bef1948131c
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-10-02 20:14:01 +0800

    改个版本试试

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='rimo_utils',
-    version='0.0.1',
+    version='1.0.0',
     author='RimoChan',
     author_email='the@librian.net',
     description='RimoChan util.',
