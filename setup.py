c5834761525f558ffdca81fa3d386087b4567c1b
RimoChan <RimoChan@stu.xmu.edu.cn>
2020-10-17 12:46:07 +0800

    加入子包

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import setuptools
 
 setuptools.setup(
     name='rimo_utils',
-    version='1.4.0',
+    version='1.4.1',
     author='RimoChan',
     author_email='the@librian.net',
     description='RimoChan util.',
     long_description='喵喵喵！',
     long_description_content_type='text/markdown',
     url='https://github.com/RimoChan/rimo_utils',
-    packages=['rimo_utils'],
+    packages=['rimo_utils', 'rimo_utils.cef_tools'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
     ],
     install_requires=[
-        'chardet==3.0.4',   
+        'chardet==3.0.4',
     ],
     python_requires='>=3.6',
 )
